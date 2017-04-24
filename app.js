aaaa4f29873784d850860c6eeab347d56939c90f
Brian McFarlane <bmcfarlane82@gmail.com>
2017-04-24 13:24:18 -0700

    pulling new repo

---
+++ app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var studentNames = [];
+
+
+
+function Student (firstName, lastName, imgSrc, linkdIn, gitHubURL) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.imgSrc = imgSrc;
+  this.linkdIn = linkdIn;
+  this.gitHubURL = gitHubURL;
+  
+}
+
+function StudentGroupProject ()
+
+Student.prototype.studentID = function() {
+  Math.random() + '' + Math.random();
+  return;
+};
