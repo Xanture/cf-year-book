230c4a87d3c589f59e77351d1b3a8121f9466495
Fortunato <fortunato.maycotte@gmail.com>
2017-04-25 18:07:13 -0700

    CSS start

--- displaypicture.js
+++ displaypicture.js
@@ -8,7 +8,6 @@ try{
 }
 for (var i = 0; i < studentList.length; i++){
 
-  // var addImage1 = document.createElement('div');
   var addImage = document.createElement('img');
   addImage.src = studentList[i].imgSrc;
   app.appendChild(addImage);
